0f443f8702a87b94dd86bb2cb3dcd09e7f1e55c5
Naman Singh <snaman431@gmail.com>
2022-01-22 01:18:49 +0530

    completed profile page

--- src/layout/Sidebar/SidebarProfile.js
+++ src/layout/Sidebar/SidebarProfile.js
@@ -2,16 +2,19 @@ import React from 'react';
 import Card from '../../layout/Card/Card';
 import avatar from './avatar.png';
 import styles from './SidebarProfile.module.css';
+import { Link } from 'react-router-dom';
 
 const SidebarProfile = () => {
   return (
-    <Card className={styles.sidebarProfile}>
-      <img src={avatar} alt='John Smith' className={styles.avatar} />
-      <p className={styles.sidebarName}>
-        John
-        <br /> Smith
-      </p>
-    </Card>
+    <Link to='/me' className={styles.textLink}>
+      <Card className={styles.sidebarProfile}>
+        <img src={avatar} alt='John Smith' className={styles.avatar} />
+        <p className={styles.sidebarName}>
+          John
+          <br /> Smith
+        </p>
+      </Card>
+    </Link>
   );
 };
 
