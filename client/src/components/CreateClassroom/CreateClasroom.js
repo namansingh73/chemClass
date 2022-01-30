ab719d468cd2cc26f92c1582d967781b86c671ce
Arhant Jain <arhantjain2@gmail.com>
2022-01-30 16:49:04 +0530

    Create clasroom done

---
+++ client/src/components/CreateClassroom/CreateClasroom.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import axios from 'axios';
+import alertActions from '../../store/alert/alert-actions';
+import PopupCard from '../../layout/PopupCard/PopupCard';
+import Input from '../../utils/Input/Input';
+import Button from '../../utils/Button/Button';
+import styles from './CreateClasroom.module.css';
+
+const CreateClassroom = (props) => {
+  const dispatch = useDispatch();
+  const [classroomName, setClassroomName] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    setLoading(true);
+
+    try {
+      const data = { name: classroomName };
+      await axios.post('/api/v1/classrooms/', data);
+      dispatch(
+        alertActions.alert({
+          alertType: 'Success',
+          info: 'Classroom created successfully!',
+        })
+      );
+      props.onClose();
+    } catch (err) {
+      if (err.response) {
+        dispatch(
+          alertActions.alert({
+            alertType: 'Error',
+            info: err.response.data.message,
+          })
+        );
+      } else {
+        dispatch(
+          alertActions.alert({
+            alertType: 'Error',
+            info: 'Something went wrong!',
+          })
+        );
+      }
+    }
+    setLoading(false);
+  };
+
+  return (
+    <PopupCard onClose={props.onClose}>
+      <form className={styles.form} onSubmit={submitHandler}>
+        <h2 className={styles.heading}>Create new Class!</h2>
+        <p>Welcome back!</p>
+        <Input
+          id='clasroomName'
+          type='text'
+          placeholder='Enter class name'
+          required
+          onChange={(event) => setClassroomName(event.target.value)}
+          value={classroomName}
+        />
+        <Button color='blue' rounded disabled={loading} loading={loading}>
+          <i className='fab fa-telegram-plane'></i> Create
+        </Button>
+      </form>
+    </PopupCard>
+  );
+};
+
+export default CreateClassroom;
--- client/src/layout/Navbar/Notifications.js
+++ client/src/layout/Navbar/Notifications.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import alertActions from '../../store/alert/alert-actions';
+import Tooltip from '../../utils/Tooltip/Tooltip';
+import CreateClassroom from '../../components/CreateClassroom/CreateClasroom';
 import styles from './Notifications.module.css';
 
 const Notifications = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const [displayCreateClassPopup, setDisplayCreateClassPopup] = useState(false);
 
   const logoutHandler = async () => {
     if (!window.confirm('Are you sure you want to logout?')) {
@@ -30,9 +34,18 @@ const Notifications = () => {
 
   return (
     <div>
-      <button className={styles.notificationBtn}>
-        <i className='far fa-bell'></i>
-      </button>
+      {displayCreateClassPopup && (
+        <CreateClassroom onClose={() => setDisplayCreateClassPopup()} />
+      )}
+      <Tooltip
+        DomElement={'button'}
+        hoverText={'Create Class'}
+        className={styles.notificationBtn}
+        direction={'left'}
+        onClick={() => setDisplayCreateClassPopup(true)}
+      >
+        <i className='fas fa-plus'></i>
+      </Tooltip>
       <button className={styles.notificationBtn} onClick={logoutHandler}>
         <i className='fas fa-sign-out-alt'></i>
       </button>
